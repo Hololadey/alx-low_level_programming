284719f95e9839b76964c6c0642263c313c3aa52
hololadey <hololade109@gmail.com>
2022-10-17 08:25:30 -0700

    initial

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == 'e' || i == 'q')
-continue;
-putchar(i);
+			continue;
+		putchar(i);
 	}
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
